d02c30ebb65805c1d2348d547040dc039b31b855
serhatbek <serhat.bekk@gmail.com>
2022-08-15 10:13:49 +0300

    React Intl - Kurulum

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import './App.css';
+import { useState } from 'react';
+import { IntlProvider, FormattedMessage, FormattedNumber } from 'react-intl';
+
+const messages = {
+  'tr-TR': {
+    title: 'Merhaba Dünya',
+    desc: 'Sor, hedefini bul, kimseyi takma',
+  },
+  'en-US': {
+    title: 'Hello World',
+    desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.',
+  },
+};
 
 function App() {
-  return <div className='App'>localization</div>;
+  const [lang, setLang] = useState('tr-TR');
+  return (
+    <div className='App'>
+      <IntlProvider messages={messages[lang]}>
+        <FormattedMessage id='title' />
+        <p>
+          <FormattedMessage id='desc' />
+        </p>
+        <button onClick={() => setLang('tr-TR')}>TR</button>
+        <button onClick={() => setLang('en-US')}>EN</button>
+      </IntlProvider>
+    </div>
+  );
 }
 
 export default App;
